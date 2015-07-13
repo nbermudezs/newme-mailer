79e5f34bf16bbc256e8520922145da6418b34ce9
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-07-12 22:45:23 -0600

    NM-6 Add endpoint to receive event reminder scheduling requests

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'NewMe Mailer' });
 });
 
+router.post('/api/v1/schedule/event-reminder', function(req, res) {
+  var reminder = req.body;
+  console.log(reminder);
+  res.end('Thanks for trying');
+});
+
 module.exports = router;
