6aae0e7cccb72fbb3f9ebd8056602a5b5dda4741
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-07-12 23:37:24 -0600

    NM-6 Add initial setup to use agenda

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var agenda = require('../worker.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NewMe Mailer' });
 });
 
-router.post('/api/v1/schedule/event-reminder', function(req, res) {
+router.post('/api/v1/schedule/event-reminder', function(req, res, next) {
   var reminder = req.body;
   console.log(reminder);
+
+  agenda.now('event reminder', reminder);
   res.end('Thanks for trying');
 });
 
